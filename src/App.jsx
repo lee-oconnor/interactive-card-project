0ed3886c209ac173805fe79e15b1a55a02785a2e
lee-o <oconnor.sherri@gmail.com>
2023-01-20 17:22:20 -0500

    Styles for cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,34 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div class="container">
-      <header class="header-div">
-        <div class="card-back">
-          <div class="black-stripe"></div>
-          <div class="grey-stripe">
-            <span class="grey-stripe-span">000</span>
+    <div className="container">
+      <header className="header-div">
+        
+          <div className="card-back">
+            <div className="black-stripe"></div>
+            <div className="grey-stripe">
+              <span className="grey-stripe-span">000</span>
+            </div>
           </div>
-        </div>
-        <div class="card-front">
-          <div class="logo"></div>
-          <div class="card-number"></div>
-          <div class="card-name"></div>
-          <div class="card-exp"></div>
-        </div>
+          <div className="card-front">
+            <div className="logo"></div>
+            <div className="card-number"></div>
+            <div className="card-name"></div>
+            <div className="card-exp"></div>
+          </div>
+       
       </header>
 
-      <form class="card-details">
-        <input class="cardholder-name" />
-        <input class="cardholder-number" />
-        <input class="exp-date-month" />
-        <input class="exp-date-year" />
-        <input class="cvc" />
-        <button class="confirm-btn"></button>
-      </form>
+      <div className="main-wrapper">
+        <form className="card-details">
+          <input className="cardholder-name" />
+          <input className="cardholder-number" />
+          <input className="exp-date-month" />
+          <input className="exp-date-year" />
+          <input className="cvc" />
+          <button className="confirm-btn"></button>
+        </form>
+      </div>
     </div>
   );
 }
