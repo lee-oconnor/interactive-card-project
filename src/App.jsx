9ee16ee661fe1a8f51b1f732f3530c9a6fddaf80
lee-o <oconnor.sherri@gmail.com>
2023-01-20 11:54:57 -0500

    Initial set-up

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div class="container">
+      <header class="header-div">
+        <div class="card-back">
+          <div class="black-stripe"></div>
+          <div class="grey-stripe">000</div>
+        </div>
+        <div class="card-front">
+          <div class="logo"></div>
+          <div class="card-number"></div>
+          <div class="card-name"></div>
+          <div class="card-exp"></div>
+        </div>
+      </header>
+
+      <form class="card-details">
+        <input class="cardholder-name" />
+        <input class="cardholder-number" />
+        <input class="exp-date-month" />
+        <input class="exp-date-year" />
+        <input class="cvc" />
+        <button class="confirm-btn"></button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
